ed66a66e0155566de3517b7823e5a187b2990b4a
milagrosVillalba <soledad110203@gmail.com>
2024-08-05 16:35:08 -0300

    Carge los datos ficticios de mi archivo CSV a mi tabla

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+import pandas as pd
+
 #conexion a la base de datos
 
 conne= mysql.connector.connect(
@@ -29,3 +31,27 @@ cursor.execute("""
 
 """)
 conne.commit()
+
+#Leer los datos ficticios de el archivo CSV
+
+df= pd.read_csv('DatosTabla.csv')
+
+#Conectar a mysql
+conne= mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='CompanyData'
+)
+cursor= conne.cursor()
+
+#insertar los datos en la tabla 
+
+for _, row in df.iterrows():
+    cursor.execute("""
+        INSERT INTO EmployeePerformance (employee_id, department, performance_score, years_with_company, salary)
+        VALUES (%s, %s, %s, %s, %s)
+    """, (row['employee_id'], row['department'], row['performance_score'], row['years_with_company'], row['salary']))
+
+conne.commit()
+conne.close()
